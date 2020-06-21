e2e5200988dd9205584a00d463d2991f310804f6
NL <NL@NLs-MacBook-Air.local>
2020-06-21 18:53:44 -0400

    displaying results from api

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 function getWeather(numInput) {
         fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${numInput}&,=usa&appid=4c2c8c79b7c3a9598be47c0b836bfd42`)
         .then(response => response.json())
-        .then(responseJson => console.log(responseJson))
-        .catch(error =>console.log(error));
-    
+        .then(responseJson => displayWeatherResults(responseJson))
+        .catch(error =>console.log(error));  
 }   
+
+function displayWeatherResults(responseJson) {
+        console.log(responseJson);
+        $('.weatherResults').append(`
+           <p>${responseJson.name}</p>
+           <p>${responseJson.weather[0].description}</p>
+           `);
+    }
 
 function getQuotes() {
         fetch(`https://type.fit/api/quotes`)
         .then(response => response.json())
-        .then(responseJson => console.log(responseJson))
+        .then(responseJson => displayQuoteResults(responseJson))
         .catch(error =>console.log(error));
     
 }   
 
+function displayQuoteResults(responseJson) {
+        console.log(responseJson);
+      for (let i = 0; i <responseJson.length; i++){
+        //console.log(i);
+        let output = responseJson[i];
+        //console.log(output);
+        $('.quoteResults').append(`
+          <p>${output.text}</p>
+          <p>${output.author}</p>
+           `);
+        }
+    }
 
-// $(getWeather('07650'));
-$(getQuotes());
+ $(getWeather('07650'));
+ //$(getQuotes());
